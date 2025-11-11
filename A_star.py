e28912b5455235c0a43cc3f9d95177d153cb9a74
Gabriel <gabrielvernizi@hotmail.com>
2025-11-11 11:42:24 -0300

    Fixing some mistakes; Display working;

--- A_star.py
+++ A_star.py
@@ -1,6 +1,6 @@
 import pygame
 import math
-from collections import deque
+import heapq as hq
 from Spot import *
 
 WIDTH = 1200
@@ -13,15 +13,19 @@ def heuristc(p1:Spot,p2:Spot) -> (int):
     x2,y2 = p2.get_pos_grid()
     return abs(x1-x2) + abs(y1-y2)
 
+def algorithm(draw,grid,start,end):
+    pass
+
 def make_grid(rows,cols,width=WIDTH,height=HEIGHT):
-    grid = [];
+    grid = []
     gap_col = width//cols
     gap_rows = height//rows
+
     for i in range(rows):
         grid.append([])
         for j in range(cols):
-            grid[i].append(Spot(i,j,height,width,rows))
-            #print(f"x: {i*gap_rows}, y: {j*gap_col}") # Debugging
+            grid[i].append(Spot(i,j,gap_rows,gap_col,rows,cols))
+            # print(f"spot[{i}][{j}] -> x: {i*gap_rows}, y: {j*gap_col}") # Debugging
         print()
 
     return grid
@@ -40,55 +44,145 @@ def draw_grid(window,rows,cols,width=WIDTH,height=HEIGHT):
 def draw(win,grid,rows,cols,width=WIDTH,height=HEIGHT):
     win.fill(WHITE)
 
+    I = 0
     for row in grid:
         for spot in row:
+            
+            # if I % 3 == 0:
+            #     I += 1
+            #     spot.color = BLUE
+            #     # print(f"Color: {spot.color}")
+            # if I % 3 == 1:
+            #     I += 1
+            #     spot.color = RED
+            #     # print(f"Color: {spot.color}")
+            # if I % 3 == 2:
+            #     I += 1
+            #     spot.color = ORANGE
+            #     # print(f"Color: {spot.color}")
+
             spot.draw(win)
 
     draw_grid(win,rows=rows,cols=cols,width=width,height=height)
     pygame.display.update()
 
 def get_clicked_pos(pos,rows,cols):
-    gap_row = WIDTH//rows
-    gap_col = HEIGHT/cols
+
+
+    gap_row = HEIGHT//rows
+    gap_col = WIDTH//cols
 
     y,x = pos
-
-    row,col = x//gap_row,y//gap_col
-
+    row,col = x//gap_col, y//gap_row
+ 
     return row,col
 
 
-pygame.init()
-clock = pygame.time.Clock()
-running = True
-
-const = WIDTH/HEIGHT
-print(const)
-ROWS = 60
-COLS = 60 #int(5/3 * ROWS)
-
-init_grid = make_grid(ROWS,COLS)
-
-def print_grid(grid):
+# Debugging
+def print_grid(grid,ROWS,COLS):
     for i in range(ROWS):
         for j in range(COLS):
-            print(init_grid[i][j])
+            print(grid[i][j].get_pos())
+
+
+def main():
+    ROWS = 60
+    COLS = math.floor(5/3 * ROWS)
+
+    start = None
+    end = None
+
+    started = False
+
+    grid = make_grid(ROWS,COLS)  
     
+    pygame.init()
+    clock = pygame.time.Clock()
+    running = True
 
 
-draw(WINDOW,init_grid,ROWS,COLS)
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            print("fechou")
-            running = False
 
-    
+    while running:
+        draw(WINDOW,grid,rows=ROWS,cols=COLS,width=WIDTH,height=HEIGHT)
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        #draw(WINDOW,grid,ROWS,COLS)
 
-    clock.tick(60)  # limits FPS to 60
 
-pygame.quit()
+        for event in pygame.event.get():
+            
 
+            if event.type == pygame.QUIT:
+                print("fechou")
+                running = False
+
+            if started:
+                # A* Algorithm
+                continue
+            
+            # Left mouse click
+            if pygame.mouse.get_pressed()[0]:
+                row,col = get_clicked_pos(pygame.mouse.get_pos(),ROWS,COLS)
+                try:
+                    spot = grid[row][col]
+                except IndexError as e:
+                    print(f"row: {row}, col: {col}")
+                    running=False
+                    raise(e)
+                
+                if not start:
+                    start = spot
+                    start.make_start()
+                
+                elif not end and spot != start:
+                    end = spot
+                    end.make_end()
+                else:
+                    if spot != start and spot != end:
+                        spot.make_barrier()
+
+            # Scroll Click
+            if pygame.mouse.get_pressed()[1]: 
+                row,col = get_clicked_pos(pygame.mouse.get_pos(),ROWS,COLS)
+                try:
+                    spot = grid[row][col]
+                except IndexError as e:
+                    print(f"row: {row}, col: {col}")
+                    running=False
+                    raise(e)
+
+                spot.update_neighbor(grid)
+                spot.color_neighbors(grid,ORANGE)
+               
+            # Right mouse click
+            if pygame.mouse.get_pressed()[2]:
+                row,col = get_clicked_pos(pygame.mouse.get_pos(),ROWS,COLS)
+                try:
+                    spot = grid[row][col]
+                except IndexError as e:
+                    print(f"row: {row}, col: {col}")
+                    running=False
+                    raise(e)
+
+                spot.reset()
+                if spot == start:
+                    start = None
+                elif spot == end:
+                    end = None
+
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not started:
+                    for row in grid:
+                        for spot in row:
+                            spot.update_neighbor()
+
+                    algorithm(lambda: draw(WINDOW,grid,rows=ROWS,cols=COLS,width=WIDTH,height=HEIGHT),grid,start,end)
+
+        clock.tick(60)  # limits FPS to 60
+
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
--- Spot.py
+++ Spot.py
@@ -1,7 +1,7 @@
 import pygame
 
 RED = (255, 0, 0) # Not Path
-GREEN = (0, 255, 0) # Path
+GREEN = (0, 255, 0) # Final Path
 BLUE = (0, 255, 0) # Start
 PURPLE = (128, 0, 128) # End
 YELLOW = (255, 255, 0) 
@@ -12,20 +12,21 @@ WHITE = (255, 255, 255) # Default
 BLACK = (0, 0, 0) # Barrier
 
 class Spot:
-    def __init__(self,row,col,height,width,total_rows):
+    def __init__(self,row,col,spot_height,spot_width,total_rows,total_cols):
         self.row = row;
         self.col = col;
 
-        self.height = height;
-        self.width = width;
+        self.spot_height = spot_height;
+        self.spot_width = spot_width;
 
-        self.x = row * width;
-        self.y = col * height;
+        self.x = col * spot_width;
+        self.y = row * spot_height;
 
-        self.color = RED  
+        self.color = WHITE;  
         self.neighbors = [];
         self.total_rows = total_rows;
-
+        self.total_cols = total_cols;
+    
     def get_pos(self):
         return self.row,self.col;
     
@@ -67,12 +68,30 @@ class Spot:
 
     # Draw 
     def draw(self,window):
-        pygame.draw.rect(window,self.color,(self.x,self.y,self.width,self.height))
+        pygame.draw.rect(window,self.color,(self.x,self.y,self.spot_width,self.spot_height))
 
     # The algorithm doesn't search in diagonal, i.e, we only need to focus on adjacent neighbors
     def update_neighbor(self,grid):
+        self.neighbors = []
         # spot[x-1][y]
-        pass
+        if self.col > 0 and not (grid[self.row][self.col-1].is_barrier()):
+            self.neighbors.append(grid[self.row][self.col-1])
+        # spot[x+1][y]
+        if self.col < self.total_cols - 1 and not (grid[self.row][self.col+1].is_barrier()):
+            self.neighbors.append(grid[self.row][self.col+1])
+        # spot[x][y-1]
+        if self.row > 0 and not (grid[self.row-1][self.col].is_barrier()):
+            self.neighbors.append(grid[self.row-1][self.col])
+        # spot[x][y+1]
+        if self.row < self.total_rows -1 and not (grid[self.row + 1][self.col].is_barrier()):
+            self.neighbors.append(grid[self.row+1][self.col])
+
+        return
+    
+    def color_neighbors(self,grid,COLOR):
+        for spot in self.neighbors:
+            spot.color = COLOR
+
 
     def __lt__(self,other):
         return False
